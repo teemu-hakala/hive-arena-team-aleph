fe68d9417a084186b05ab3bd3e912e559fd4422f
Natalia Samoilova <samoilova.natalia.n@gmail.com>
2022-03-21 15:54:30 +0200

    Copy old version

---
+++ old_versions/team-aleph-agent-monday/bees.c
@@ -0,0 +1,50 @@
+#include "team-aleph.h"
+
+command_t	choose_action(agent_info_t info, t_cell_history grid[NUM_ROWS][NUM_COLS], t_bees *bees)
+{
+	switch(bees->bees[info.bee].role) {
+		case SCOUT:
+			if ((info.player == 0 && bees->bees[info.bee].coords.col >= bees->forage_distance)
+				|| (info.player == 1 && bees->bees[info.bee].coords.col < bees->forage_distance))
+			{
+				if (info.bee == 1 || info.bee == 3)
+				{
+					bees->bees[info.bee].role = ATTACKER;
+					bees->bees[info.bee].target = best_attack_target(info.player, info.bee);
+					return (best_scout_route(grid, &bees->bees[info.bee], info.player));
+				}
+				else
+				{
+					bees->bees[info.bee].role = FORAGER;
+					bees->bees[info.bee].target.row = -1;
+					return (best_forage_route(info, grid, bees));
+				}
+			}
+			else
+			{
+				return (best_scout_route(grid, &bees->bees[info.bee], info.player));
+			}
+		case FORAGER:
+			if ((((info.player == 0 && bees->bees[info.bee].coords.col < bees->forage_distance / 2)
+				|| (info.player == 1 && bees->bees[info.bee].coords.col >= bees->forage_distance * 3 / 2))
+				&& bees->hive_foragers < 1)
+				|| no_flowers_in_forage_area(grid, bees->forage_distance, bees->hive_forage_distance, info.player))
+			{
+				bees->bees[info.bee].role = HIVE_FORAGER;
+				bees->hive_foragers++;
+				return (best_forage_route(info, grid, bees));
+			}
+			else
+			{
+				return (best_forage_route(info, grid, bees));
+			}
+		case HIVE_FORAGER:
+			return (best_forage_route(info, grid, bees));
+		case ATTACKER:
+			return (best_attack_route(grid, &bees->bees[info.bee], info, bees));
+		case WAYPOINT:
+			return (best_waypoint_route(grid, &bees->bees[info.bee], info.player));
+		default:
+			return (best_scout_route(grid, &bees->bees[info.bee], info.player));
+	}
+}
---
+++ old_versions/team-aleph-agent-monday/team-aleph.h
@@ -0,0 +1,104 @@
+#ifndef TEAM_ALEPH_H
+#define TEAM_ALEPH_H
+#include <time.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <strings.h>
+#include <stdbool.h>
+#include "agent.h"
+
+typedef enum grid_cell_e
+{
+	NO_INFO,
+    EMPTY_ALEPH,
+	BEE_ALEPH,
+    BEE_ALEPH_0,
+	BEE_ALEPH_1,
+	BEE_ALEPH_2,
+	BEE_ALEPH_3,
+	BEE_ALEPH_4,
+    BEE_ENEMY,
+	BEE_ALEPH_WITH_FLOWER,
+    BEE_ALEPH_0_WITH_FLOWER,
+	BEE_ALEPH_1_WITH_FLOWER,
+	BEE_ALEPH_2_WITH_FLOWER,
+	BEE_ALEPH_3_WITH_FLOWER,
+	BEE_ALEPH_4_WITH_FLOWER,
+    BEE_ENEMY_WITH_FLOWER,
+    FLOWER_ALEPH,
+	TARGET_FLOWER,
+    WALL_ALEPH,
+	WALL_ENEMY,
+    HIVE_ALEPH,
+    HIVE_ENEMY
+} grid_cell_t;
+
+static const coords_t stack_cells[] = {
+    {0, 0},
+    {0, 2},
+    { 1, 1},
+    { 2, 0},
+    { 2, 2},
+};
+
+typedef struct
+{
+	grid_cell_t cell;
+	int			turn;
+	bool		is_stack;
+	bool		target_stack;
+} t_cell_history;
+
+typedef enum e_role
+{
+	SCOUT,
+	FORAGER,
+	HIVE_FORAGER,
+	ATTACKER,
+	BUILDER,
+	WAYPOINT
+} t_role;
+typedef struct
+{
+	t_role		role;
+	coords_t	coords;
+	bool		with_flower;
+	coords_t	target;
+} t_bee;
+
+typedef struct
+{
+	t_bee	bees[NUM_BEES];
+	int		attackers;
+	int		hive_foragers;
+	int		forage_distance;
+	int		hive_forage_distance;
+	coords_t	top_left_stack[2];
+}	t_bees;
+
+void	update_grid(agent_info_t info, \
+	t_cell_history grid[NUM_ROWS][NUM_COLS], t_bee bees[NUM_BEES]);
+command_t	best_scout_route(t_cell_history grid[NUM_ROWS][NUM_COLS], t_bee *bee, int player);
+void	initialize_bees(t_bee bees[NUM_BEES], int player);
+bool	is_aleph_bee_with_flower(grid_cell_t bee);
+int	distance_between_points(coords_t coord0, coords_t coord1);
+coords_t	hive_coords(int player);
+command_t	best_forage_route(agent_info_t info, \
+	t_cell_history grid[NUM_ROWS][NUM_COLS], \
+	t_bees *bees);
+int	get_forage_distance(int player);
+int	get_hive_forage_distance(int player);
+command_t	choose_action(agent_info_t info, t_cell_history grid[NUM_ROWS][NUM_COLS], t_bees *bees);
+void	create_stacks(t_cell_history grid[NUM_ROWS][NUM_COLS], int player, t_bees *bees);
+bool	no_flowers_in_forage_area(t_cell_history grid[NUM_ROWS][NUM_COLS], int forage_distance, int hive_forage_distance, int player);
+command_t	best_waypoint_route(t_cell_history grid[NUM_ROWS][NUM_COLS], t_bee *bee, int player);
+command_t best_attack_route(t_cell_history grid[NUM_ROWS][NUM_COLS], t_bee *bee, agent_info_t info, t_bees *bees);
+int	get_info_from_coord(coords_t current, t_cell_history grid[NUM_ROWS][NUM_COLS]);
+coords_t	best_attack_target(int player, int index);
+bool	coords_equal(coords_t coords0, coords_t coords1);
+bool	enemy_bee_is_close_and_adjacent_flower(t_cell_history grid[NUM_ROWS][NUM_COLS], t_bee *current_bee);
+bool	enemy_bee_is_close(t_cell_history grid[NUM_ROWS][NUM_COLS], t_bee *current_bee);
+
+#endif
